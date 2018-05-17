20a75e6d980dd8edd9ef718a4bcaa907cf4ce83a
philippasigl <philippa@sigl-gloeckner.de>
2018-05-17 14:09:47 +0100

    adding trend data

--- static/src/network.js
+++ static/src/network.js
@@ -281,7 +281,7 @@ const select_edgeSizeKey = () => {
 const set_change = () => {
     let edgeSize  
     _edges.map((edge) => {
-        let compEdge = _edges.find((it) => {return it['dateID']==_comparisonDate && it['from']==edge['from'] && it['to']==edge['to']})
+        let compEdge = _edges.find(it => it['dateID']==_comparisonDate && it['from']==edge['from'] && it['to']==edge['to'])
         compEdge == undefined ? edgeSize = 0 : edgeSize = edge['absValue'] - compEdge['absValue']
 
         if (edgeSize > 0) edge['trend'] == 'increased'
@@ -308,7 +308,7 @@ const set_edgeRange = () => edgeSlider.noUiSlider.on('change', (values) => {
 */
 
 const set_edgeChangeValue = () => edgeChangeSlider.noUiSlider.on('change',(values) => {
-    _comparisonDate=parseFloat(values[0].dateID)
+    _comparisonDate=parseFloat(values[0])
     draw()
 })
 
